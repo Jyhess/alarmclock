8b33ff25555f40c795c66b9e254fc1a97ec38abf
Jyhess <jyhess@gmail.com>
2021-05-24 22:56:09 +0200

    Add RTC

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
 #include <TM1637Display.h>
+#include <Wire.h> // must be included here so that Arduino library object file references work
+#include <RtcDS3231.h>
 
 // Module connection pins (Digital Pins)
 #define CLK 2
 #define DIO 3
+#define SDA 4
+#define SCL 5
 
 // The amount of time (in milliseconds) between tests
 #define TEST_DELAY   2000
 
 TM1637Display tm_1637(CLK, DIO);
+RtcDS3231<TwoWire> Rtc(Wire);
 
 uint8_t g_brightness = 0;
 
-void display(uint8_t time)
+void display(uint16_t time)
 {
   // uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
   // data[0] = tm_1637.encodeDigit(0);
@@ -25,18 +30,127 @@ void display(uint8_t time)
 	tm_1637.showNumberDecEx(time, 64, false);
 }
 
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+
+void printDateTime(const RtcDateTime& dt)
+{
+    char datestring[20];
+
+    snprintf_P(datestring, 
+            countof(datestring),
+            PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
+            dt.Month(),
+            dt.Day(),
+            dt.Year(),
+            dt.Hour(),
+            dt.Minute(),
+            dt.Second() );
+    Serial.print(datestring);
+}
+
 void setup()
 {
+  Serial.begin(57600);
+
+  Serial.print("compiled: ");
+  Serial.print(__DATE__);
+  Serial.println(__TIME__);
+  
+  Rtc.Begin();
+
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+  printDateTime(compiled);
+  Serial.println();
+
+  if (!Rtc.IsDateTimeValid()) 
+  {
+      if (Rtc.LastError() != 0)
+      {
+          // we have a communications error
+          // see https://www.arduino.cc/en/Reference/WireEndTransmission for 
+          // what the number means
+          Serial.print("RTC communications error = ");
+          Serial.println(Rtc.LastError());
+      }
+      else
+      {
+          // Common Causes:
+          //    1) first time you ran and the device wasn't running yet
+          //    2) the battery on the device is low or even missing
+
+          Serial.println("RTC lost confidence in the DateTime!");
+
+          // following line sets the RTC to the date & time this sketch was compiled
+          // it will also reset the valid flag internally unless the Rtc device is
+          // having an issue
+
+          Rtc.SetDateTime(compiled);
+      }
+  }
+
+  if (!Rtc.GetIsRunning())
+  {
+      Serial.println("RTC was not actively running, starting now");
+      Rtc.SetIsRunning(true);
+  }
+
+  RtcDateTime now = Rtc.GetDateTime();
+  if (now < compiled) 
+  {
+      Serial.println("RTC is older than compile time!  (Updating DateTime)");
+      Rtc.SetDateTime(compiled);
+  }
+  else if (now > compiled) 
+  {
+      Serial.println("RTC is newer than compile time. (this is expected)");
+  }
+  else if (now == compiled) 
+  {
+      Serial.println("RTC is the same as compile time! (not expected but all is fine)");
+  }
+
+  // never assume the Rtc was last configured by you, so
+  // just clear them to your needed state
+  Rtc.Enable32kHzPin(false);
+  Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone);
 }
+
 
 void loop()
 {
-  uint8_t time = 0;
+  uint16_t time = 0;
   while(true)
   {
+    if (!Rtc.IsDateTimeValid()) 
+    {
+        if (Rtc.LastError() != 0)
+        {
+            // we have a communications error
+            // see https://www.arduino.cc/en/Reference/WireEndTransmission for 
+            // what the number means
+            Serial.print("RTC communications error = ");
+            Serial.println(Rtc.LastError());
+        }
+        else
+        {
+            // Common Causes:
+            //    1) the battery on the device is low or even missing and the power line was disconnected
+            Serial.println("RTC lost confidence in the DateTime!");
+        }
+    }
+
+    RtcDateTime now = Rtc.GetDateTime();
+    printDateTime(now);
+    Serial.println();
+
+    RtcTemperature temp = Rtc.GetTemperature();
+    temp.Print(Serial);
+    // you may also get the temperature as a float and print it
+    // Serial.print(temp.AsFloatDegC());
+    Serial.println("C");
+
     display(time++);
-    delay(500);
+    delay(1000); // ten seconds
   }
 }
-
 
