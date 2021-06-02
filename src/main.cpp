91caa6c61d8910a6a06dbabfbabe8b63b46de4a4
Jyhess <jyhess@gmail.com>
2021-06-02 17:06:12 +0200

    Remove capacitive sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include <Arduino.h>
-#include "timed_capacitive_sensor.h"
 #include "rtc.h"
-#include "sun.h"
 #include "oled.h"
 
 // Module connection pins (Digital Pins)
@@ -10,8 +8,6 @@
 #define CAPACITY_2 11
 
 Rtc g_rtc;
-TimedCapacitiveSensor g_tcs(CAPACITY_1, CAPACITY_2);
-Sun g_sun(LED);
 Oled g_oled;
 long last_run_ms = 0;
 
@@ -19,9 +15,7 @@ void setup()
 {
   Serial.begin(57600);
   Serial.println("Running setup...");
-  g_sun.setup();
   g_rtc.setup();
-  g_tcs.setup();
   g_oled.setup();
   Serial.println("Setup done");
 }
@@ -29,17 +23,6 @@ void setup()
 void loop()
 {
   long now_ms = millis();
-
-  g_tcs.loop(now_ms);
-  if (g_tcs.increasing())
-  {
-    g_sun.switch_on(g_tcs.percent());
-  }
-  else if (g_tcs.decreasing())
-  {
-    g_sun.switch_off(now_ms);
-  }
-  g_sun.loop(now_ms);
 
   // Update time only once per seconds
   if (now_ms - last_run_ms > 1000 || now_ms - last_run_ms < 0)
